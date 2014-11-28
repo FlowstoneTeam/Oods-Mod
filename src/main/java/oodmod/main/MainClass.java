169c603da0a21f011689117cbc4008d28a938cdf
Glassmaker <JeanGlassmaker@mail.com>
2014-11-28 04:57:16 +0300

    Adds version implementation into mcmod.info and the Main class

--- src/main/java/oodmod/main/MainClass.java
+++ src/main/java/oodmod/main/MainClass.java
@@ -35,7 +35,7 @@ public class MainClass {
 		//Mod Info
 		public static final String MODID = "oodmod";
 		public static final String NAME = "Ood's Mod";
-		public static final String VERSION = "1.91";
+		public static final String VERSION = "${version}";
 		
 		//Achievements
 		public static Achievement achievementKroostyl;
